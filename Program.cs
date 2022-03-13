0fa40f31664ce428d088e062c23dcbb2ad8f2afc
Matt Jenkins <matt@mpj.org.uk>
2022-03-13 09:56:12 +0000

    Minimal repro

--- Program.cs
+++ Program.cs
@@ -1,25 +1,18 @@
-var builder = WebApplication.CreateBuilder(args);
+WebApplicationOptions opts = new()
+{
+    Args = args,
 
-// Add services to the container.
-builder.Services.AddRazorPages();
+    // If a folder named "wwwroot" exists, requests are preferentially served from it, disregarding this setting.
+    // This is unfortunate because I want to switch the web root path depending on environment settings, without deleting the wwwroot folder.
+    // An empty wwwroot-custom folder is also created if it does not exist. Perhaps that is undesirable behaviour also?
+    WebRootPath = "wwwroot-custom",
+};
+
+var builder = WebApplication.CreateBuilder(opts);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
+app.UseDefaultFiles();
 app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapRazorPages();
 
 app.Run();
